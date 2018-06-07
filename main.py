1cdbd3ce9d38b195aeea47c6d5504ffc535055dd
sleepwallker <www.vetal.ua9513@gmail.com>
2018-06-07 11:57:31 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import sys
+import tornado.ioloop
+import tornado.web
+import os.path
+import json
+import main_nn
+
+
+class MainHandler(tornado.web.RequestHandler):
+
+    def post(self):
+        self.set_header("Content-Type", "text/plain")
+        self.request.body()
+        self.write(json.dumps(main_nn.result))
+
+        #sys.stdout.write(('POST REQUEST: {}'.format(self.request.body)))
+        #sys.stdout.write('GET REQUEST: {}'.format(main_nn))
+
+
+# This tells tornado where to find the static files
+setting = dict(
+    template_path=os.path.join(os.path.dirname(__file__), "templates"),
+    static_path=os.path.join(os.path.dirname(__file__), "static"),
+    debug=True
+)
+
+# r"/" == root website address
+apllication = tornado.web.Application([
+    (r"/", MainHandler)
+], **setting)
+
+# Start the server at port n
+if __name__ == "__main__":
+    print('Server Running...')
+    print('Press ctrl + c to close')
+    print('request accepted')
+    print('close window')
+    apllication.listen(8889)
+    tornado.ioloop.IOLoop.instance().start()
---
+++ main_nn.py
@@ -0,0 +1,58 @@
+import os
+import cv2
+import dlib
+import numpy as np
+
+from wide_resnet import WideResNet
+from keras.utils.data_utils import get_file
+
+WEIGHT_FILE = "./model/age_and_gender.hdf5"
+
+DEPTH = 16
+WIDTH = 8
+
+IMAGE_SIZE = 64
+
+if not WEIGHT_FILE:
+    weight_file = get_file("age_and_gender.hdf5", origin='images', cache_subdir="model", cache_dir=os.path.dirname(os.path.abspath(__file__)))
+
+detector = dlib.get_frontal_face_detector()
+
+model = WideResNet(IMAGE_SIZE, DEPTH, WIDTH)()
+model.load_weights(WEIGHT_FILE)
+
+input_image = cv2.imread('./images/vetaliy.jpeg')
+grayscale = cv2.cvtColor(input_image, cv2.COLOR_BGR2GRAY)
+img_height, img_weight = np.shape(grayscale)
+
+detected = detector(grayscale, 1)
+faces = np.empty((len(detected), IMAGE_SIZE, IMAGE_SIZE, 3))
+
+if len(detected) > 0:
+    for i, d in enumerate(detected):
+        x1, y1, x2, y2, w, h = d.left(), d.top(), d.right() + 1, d.bottom() + 1, d.width(), d.height()
+        xw1 = max(int(x1 - 0.4 * w), 0)
+        yw1 = max(int(y1 - 0.4 * h), 0)
+        xw2 = min(int(x2 + 0.4 * w), img_weight - 1)
+        yw2 = min(int(y2 + 0.4 * h), img_height - 1)
+        cv2.rectangle(input_image, (x1, y1), (x2, y2), (0, 0, 255), 2)
+        faces[i, :, :, :] = cv2.resize(input_image[yw1:yw2 + 1, xw1:xw2 + 1, :], (IMAGE_SIZE, IMAGE_SIZE))
+
+    result = []
+
+    results = model.predict(faces)
+    predicted_genders = results[0]
+    ages = np.arange(0, 101).reshape(101, 1)
+    predicted_ages = results[1].dot(ages).flatten()
+
+    for i, d in enumerate(detected):
+        age, gender = predicted_ages[i], "F" if predicted_genders[i][0] > 0.5 else "M"
+
+        result.append({'face{}'.format(i): [x1, y1, x2, y2],
+                       'age': int(age),
+                       'gender': gender})
+
+        print(result)
+
+cv2.imshow("result", input_image)
+cv2.waitKey(0)
